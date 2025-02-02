392a3a499e509685096c2ccacbbc5231de55a332
Jonathan <jonathancornejox@gmail.com>
2025-02-02 08:12:04 -0800

    comments of source code for student struct file

--- readingExercises/Chapter2/sortStudentsStruct.cpp
+++ readingExercises/Chapter2/sortStudentsStruct.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 #include <string>
 
+// struct data type of Student
 struct Student {
   std::string name;
   int age;
   double GPA;
 
+  // display of student information
   void displayStudentGPA() {
     std::cout << "\n Name: " << name << " - " << "Age: " << age << " - "
               << "GPA: " << GPA << std::endl;
   }
 };
 
+// loop through the data and compare each value to swap if not in descending
+// order
 void sortStudentGPA(Student students[], int size) {
   for (int i = 0; i < size - 1; i++) {
     for (int j = i + 1; j < size; j++) {
@@ -25,6 +29,8 @@ void sortStudentGPA(Student students[], int size) {
 int main() {
   const int numStudents = 2;
   Student students[numStudents];
+
+  // loop through amount of student to prompt user for student information
 
   for (int i = 0; i < numStudents; i++) {
 
@@ -41,9 +47,12 @@ int main() {
     std::cin.ignore();
   }
 
+  // call to the function
+
   sortStudentGPA(students, numStudents);
   std::cout << "Student Details (sorted by GPA):\n";
 
+  // display of the all the student info data ordered by GPA in descending order
   for (int i = 0; i < numStudents; i++) {
     students[i].displayStudentGPA();
   }
