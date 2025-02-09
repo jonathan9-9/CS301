abe030d9e64a531ae7786fe71d8e19679f23f4cc
Jonathan <jonathancornejox@gmail.com>
2025-02-08 22:40:27 -0800

    implemented accessor functions and display info fn

--- readingExercises/Chapter3/vehicleClass.cpp
+++ readingExercises/Chapter3/vehicleClass.cpp
@@ -2,3 +2,13 @@
 #include <iostream>
 
 Vehicle::Vehicle(const std::string &c, int w) : color(c), numWheels(w) {}
+
+Vehicle::~Vehicle() {}
+
+std::string Vehicle::getColor() const { return color; }
+
+int Vehicle::getNumWheels() const { return numWheels; }
+
+void Vehicle::displayInfo() const {
+  std::cout << "Color: " << color << ", Wheels: " << numWheels;
+}
