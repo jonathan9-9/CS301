42c77a60819ddbb27393d7a7137bfc606fef2147
Jonathan <jonathancornejox@gmail.com>
2025-02-23 20:41:32 -0800

    read each word from input file and initialize item to add to the list(array)

--- assignment3/sourceCode/main.cpp
+++ assignment3/sourceCode/main.cpp
@@ -4,6 +4,26 @@
 
 int main() {
   std::ifstream inputFile("input.txt");
+  if (!inputFile) {
+    std::cerr << "Error opening file. Try again" << std::endl;
+    return 1;
+  }
+
+  UnsortedType list;
+  int arraySize;
+  std::string word;
+
+  inputFile >> arraySize; // here we will read in the size of the array
+
+  for (int i = 0; i < arraySize; i++) {
+    inputFile >> word;
+    ItemType newItem;
+    newItem.Initialize(word);
+    list.PutItem(newItem);
+  }
+
+  std::cout << "Stored List: ";
+  list.PrintList();
 
   return 0;
 }
