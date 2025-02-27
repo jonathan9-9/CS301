0c77c3e9ed361c541fd2f0d3f80e849d568fa9e1
Jonathan <jonathancornejox@gmail.com>
2025-02-26 21:47:05 -0800

    create statement list function and recursive call

--- ProgrammingLanguageConcepts/cs311-hw2.py
+++ ProgrammingLanguageConcepts/cs311-hw2.py
@@ -6,3 +6,12 @@ LIMIT_RECURSION = 2
 
 def program():
     return f"int main() {{ {create_stat_list(0)} return 0; }}"
+
+
+def create_stat_list(depth):
+    if depth >= LIMIT_RECURSION:
+        return create_assgn_stat()
+
+    first_statement = create_stat(depth + 1)
+    second_statement = create_stat(depth + 1)
+    return first_statement + second_statement
